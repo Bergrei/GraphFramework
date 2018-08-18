e88e142f9073d7ae4c6914d045338042588b5f75
Bergrei <balog.greg@gmail.com>
2018-08-18 18:51:36 +0200

    [initail] Fix missing interface methods
    Adding accidentally removed methods back into PlanarFAce interface

--- src/org/tonerds/graphframework/planargraph/PlanarFace.java
+++ src/org/tonerds/graphframework/planargraph/PlanarFace.java
@@ -1,6 +1,13 @@
 package org.tonerds.graphframework.planargraph;
 
-public interface PlanarFace{	
+public interface PlanarFace{
+
+	public void addNode(PlanarNode node);
+	
+	public boolean containsNode(PlanarNode node);
+	
+	public boolean removeNode(PlanarNode node);
+	
 	public void addEdge(PlanarNode fromnode, PlanarNode tonode, PlanarEdge edge);
 	
 	public boolean containsEdge(PlanarEdge edge);
